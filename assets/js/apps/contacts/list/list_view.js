6049cfd7967246ed99c3948d7785435742b6a84d
romaaverkin <romaaverkin@gmail.com>
2022-12-16 15:16:53 +0700

    Знакомство с триггерным методом

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -15,6 +15,13 @@ ContactManager.module("ContactsApp.List", function (List, ContactManager, Backbo
         deleteClicked: function (e) {
             e.stopPropagation();
             this.trigger("contact:delete", this.model);
+        },
+
+        remove: function () {
+            var self = this;
+            this.$el.fadeOut(function () {
+                Marionette.ItemView.prototype.remove.call(self);
+            });
         }
     });
 
