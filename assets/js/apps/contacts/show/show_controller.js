5170b76fd880c128884c53c27343bb5c7e0f55ea
RomanAverkin <romaaverkin@gmail.com>
2023-02-21 18:34:04 +0700

    Отложенный jQuerry

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -1,18 +1,21 @@
-ContactManager.module("ContactsApp.Show", function (Show, ContactManager, Backbone, Marionette, $, _) {
+ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbone, Marionette, $, _){
     Show.Controller = {
-        showContact: function (id) {
-            var contact = ContactManager.request("contact:entity", id);
-            var contactView;
-            if (contact !== undefined) {
-                contactView = new Show.Contact({
-                    model: contact
-                });
-            }
-            else {
-                contactView = new Show.MissingContact();
-            }
-
-            ContactManager.mainRegion.show(contactView);
-        }
+      showContact: function(id){
+        var fetchingContact = ContactManager.request("contact:entity", id);
+        $.when(fetchingContact).done(function(contact){
+          var contactView;
+          if(contact !== undefined){
+            contactView = new Show.Contact({
+              model: contact
+            });
+          }
+          else{
+            contactView = new Show.MissingContact();
+          }
+  
+          ContactManager.mainRegion.show(contactView);
+        });
+      }
     }
-});
+  });
+  
--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -22,25 +22,45 @@ ContactManager.module("Entities", function (Entities, ContactManager, Backbone, 
         contacts.forEach(function (contact) {
             contact.save();
         });
-        return contacts;
+        return contacts.models;
     };
 
     var API = {
-        getContactEntities: function () {
-            var contacts = new Entities.ContactCollection();
-            contacts.fetch();
-            if (contacts.length === 0) {
-                return initializeContacts();
+        getContactEntities: function(){
+          var contacts = new Entities.ContactCollection();
+          var defer = $.Deferred();
+          contacts.fetch({
+            success: function(data){
+              defer.resolve(data);
             }
-            return contacts;
+          });
+          var promise = defer.promise();
+          $.when(promise).done(function(contacts){
+            if(contacts.length === 0){
+              // if we don't have any contacts yet, create some for convenience
+              var models = initializeContacts();
+              contacts.reset(models);
+            }
+          });
+          return promise;
         },
-
-        getContactEntity: function (contactId) {
-            var contact = new Entities.Contact({ id: contactId });
-            contact.fetch();
-            return contact;
+    
+        getContactEntity: function(contactId){
+          var contact = new Entities.Contact({id: contactId});
+          var defer = $.Deferred();
+          setTimeout(function(){
+            contact.fetch({
+              success: function(data){
+                defer.resolve(data);
+              },
+              error: function(data){
+                defer.resolve(undefined);
+              }
+            });
+          }, 2000);
+          return defer.promise();
         }
-    };
+      };
 
     ContactManager.reqres.setHandler("contact:entities", function () {
         return API.getContactEntities();
